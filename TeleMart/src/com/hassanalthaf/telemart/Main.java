7f79ae3b830147a4b0f0667bf06471702c400a38
Hassan Althaf <hassan@hassanalthaf.com>
2015-12-19 17:54:51 +0530

    Initial Setup

---
+++ TeleMart/src/com/hassanalthaf/telemart/Main.java
@@ -0,0 +1,37 @@
+/*
+ * Program developed by Hassan Althaf.
+ * Copyright © 2015, Hassan Althaf.
+ * Website: http://hassanalthaf.com
+ */
+package com.hassanalthaf.telemart;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author hassan
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
